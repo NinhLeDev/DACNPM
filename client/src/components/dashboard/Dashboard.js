ad459a8392099265eb22d99c47e83e5566b4eaa3
Ninh Le <lephuocninh7118@gmail.com>
2019-02-14 20:11:03 +0700

    create enviroment react and node

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+// import Spinner from '../common/Spinner';
+
+class Dashboard extends Component {
+  componentDidMount() {
+  }
+
+  onDeleteClick(e) {
+  }
+
+  render() {
+    return (
+      <div>
+        Dashboard
+      </div>
+    );
+  }
+}
+
+Dashboard.propTypes = {
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps )(
+  Dashboard
+);
---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb://jobseeker:123123ninh@ds119085.mlab.com:19085/jobseeker',
+  secretOrKey: 'secret'
+};
